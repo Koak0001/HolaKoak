3a15839e943bd42f606d21e259ef68286a8cf3bd
koak0001 <koak0001@stud.kea.dk>
2023-09-08 12:07:54 +0200

    First Commit

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Dat23c");
+
+
+        String greeting = "Hello";
+        System.out.println(greeting);
+
+        System.out.println("Goodbye Dat23c");
+
+        String leave= "Bye";
+        System.out.println(leave);
+    }
+}
